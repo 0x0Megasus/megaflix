786ca7e7a70e4ac4a7ec856e673eaaa25b7f9f1b
0x0Megasus <0x00megasus@gmail.com>
2026-05-25 13:48:56 +0000

    Split monolithic index.css into base.css, components.css, responsive.css for maintainability

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
-import './index.css'
+import './base.css'
+import './components.css'
+import './responsive.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
